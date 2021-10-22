7efdcae5fd4eb52d531506eb7e463bbebd4e1b12
AndresGratus <91640921+AndresGratus@users.noreply.github.com>
2021-10-22 17:41:55 -0500

    Creamos dos clases Preguntas y metodos

---
+++ src/main/Metodo.java
@@ -0,0 +1,7 @@
+
+package main;
+
+//Creamos la clase metodo y agregamos sus funcionalidades
+public class Metodo {
+    
+}
---
+++ src/main/Pregunta.java
@@ -0,0 +1,12 @@
+
+package main;
+
+//Creamos la clase pregunta y sus atributos
+public class Pregunta {
+    
+   String pregunta;
+   String respuestatrue1;
+   String respuestafalse1;
+   String respuestafalse2;
+   String respuestafalse3;
+}
